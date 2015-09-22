2b4629640ec9d357bcdf50efd35a629401f6bfb1
eric <naisanza@gmail.com>
2015-09-22 15:11:11 -0400

    Added DNS resolver service

---
+++ sailboat/opt/custom/share/svc/resolvers.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Add SSH Public Key to Root Script
+
+
+# Version="1.0.0";
+# Revision="0b";
+# Script="add-ssh-pub-key-to-root.sh";
+
+resolvers=/etc/resolv.conf
+nameservers=(8.8.8.8,8.8.4.4)
+
+
+set -o xtrace
+
+. /lib/svc/share/smf_include.sh
+
+cd /
+PATH=/usr/sbin:/usr/bin:/opt/custom/bin:/opt/custom/sbin; export PATH;
+
+case "$1" in
+    'start')
+	#### Insert code to execute on startup here.
+	for (( i=0; i<${nameservers[$i]}; i++ )); do
+		cat ${nameservers[$i]} >> $resolvers
+	done
+
+	;;
+    
+    'stop')
+	### Insert code to execute on shutdown here.	
+	;;
+    
+    *)
+	echo "Usage: $0 { start | stop }"
+	exit $SMF_EXIT_ERR_FATAL
+	;;
+esac
+exit $SMF_EXIT_OK
