00c904765627163447d102d902a5e7406f006513
Eric <naisanza@gmail.com>
2015-09-24 02:52:54 -0400

    Bug fixes

--- sailboat/opt/custom/bin/debug.sh
+++ sailboat/opt/custom/bin/debug.sh
@@ -1,67 +1,66 @@
 #!/usr/bin/env bash
+function happy_term(){
 
-function spec_kvm_iso_copy(){
-
-  # Selects and copies an ISO to the KVM root for booting
-  # e.g. Windows
-
-  # Optional: Attaching virtio driver iso
-
-  ISOS=/opt/custom/iso
-  ISO=($(ls "$ISOS" | grep .iso))
-  VIRTIOS=/opt/custom/iso/virtio
-  VIRTIO=($(ls "$VIRTIOS" | grep .iso))
-
-  while true; do
-    vmadm list
-    read -p "Set UUID of the new VM: " UUID
-    if [ ! -z "$UUID" ]; then vmadm list | grep -w "$UUID"
-      if [ $? == 0 ]; then
-        vmadm stop "$UUID" -F
-        break
-        else echo "UUID not found. Check spelling and try again"; fi
-    fi
-  done
-  echo ""
-
-  echo "Select an ISO"
-  select ANSWER in "${ISO[@]}"; do
-    ISO="$ANSWER"
-    echo ""
-    echo Copying "$ISO" "==>" /zones/"$UUID"/root/
-    echo ""
-    rsync -ih --progress $ISOS/"$ISO" /zones/"$UUID"/root/
-    break
-  done
-  echo ""
-
-  echo "Boot?"
-  select ANSWER in "Yes" "No"; do
-    case $ANSWER in
-      Yes)
-        echo "Attach virtio ISO?"
-        select ANSWER in "Yes" "No"; do
-          case $ANSWER in
-            Yes)
-              echo "Select virtio iso"
-              select ANSWER in "${VIRTIO[@]}"; do
-                VIRTIO="$ANSWER"
-                vmadm boot "$UUID" order=cd,once=d cdrom=/"$ISO",ide cdrom=/"$VIRTIO",ide
-              done
-              break ;;
-
-            No) break ;;
-          esac
-        done
-
-        vmadm boot "$UUID" order=cd,once=d cdrom=/"$ISO",ide
-        break ;;
-      No) break ;;
-    esac
-  done
+print '
+ICAgICAgICAgICAgIC4ueis6KysrOjo7Kys6OisrKysrKzo6OisrOjorKzo6Ojor
+KysremF6dWdtSiwuCiAgICAgICAgIC4sNys6Oi5KK0orP0p1eEpRZ0hISEhtUWdn
+bWErSkp4LEomJitKSm1tU3l3WGsxK3pUSEgmLAogICAgICAgLDo/aSsuZFdPV0hI
+SEhISEhISEhISEhISEhIQkI5VVRZOj8rVFdZWVdISEhISEhuKz9YZSs/VDo/Li4K
+ICAgIC5KOjo/d1hnWFdIbW1tbW1tbW1tbW1tbUhXbW1tSEk6OjM6Ojo6Ojo6Ojo6
+Pz9XSG1tbWdtV1htSFNpLDo/Ky4KICAgLDo6Lit4WVkkV1dBJldQZGhYbW1tbW1t
+bW1tSEg5PUorOjo6Ojo6Ojo6Ojo6Ojo6Sm1tbWdtSFFXSEM6Ojo6OjppCiAgKzo6
+P2pRa2srK3o/UW1tSEhtbW1tbW1tbW1tbVo/ejc6Ojo6Ojo6Ojo6XjpeOjpeOjo/
+VUhtbW1tbUsrOjo6Xjo6OisuCiArOj9kSG1tbW1tbW1LV21SZFlZSG1nbW1nbWdt
+QkM/Ojo6Ojo6XjpeOjpeOjo6Ol46Ojo6Oj9YSEs6Oj8rLDo6Ojo6OjorCi46Okpt
+bW1nbW1tbW1ISm1CMzo6P0g5Oj9UbW06Ong6Ojo6Xjo6Ojo6Ojo6OjorOjo6Ol46
+Ojo6P1Q2WCs/Pz0rLl46Ojo6OgorOjo/SEhISG1tZ21tbUhrOjo6Ojo/Yzo6Pys9
+Oj9qYzo6Xjo6OjpeOjo6Xjo6Xjo6Xjo6Ojo6Ojo6Ojo/K2prUW0sLl46OisKbDo6
+Ojo6Oj9XbW1tbWc5Ojo6Ojo6Ojo6Oj9YSlhEeCsrSiw6Ljo6Ol46Ojo6Xjo6Ojo6
+Ol46Xjo6Ojo6Oj9YbW1tbVIrLDo/Cjo6Xjo6Ojo6P0hnbW1IKzo6Ol46Ol46Ojo6
+Oj8xOz86Kz9ZMzorKzo6OjpeOjo6Xjo6Xjo6OjpeOl46Ojo/V21tbW1tbW1EPwou
+Ojo6Xjo6Oj9IbW1nIz1qSTo6Ojo6Xjo6Ojo6OjpKUW1tZHg6OjorOj86Ojo6Ojo6
+Xjo6Ol46Ojo6Xjo6Oj9XbWdtbW1EOjoKICs6OjpeOjo6P0htbUQ6Pzo6Ol46Ojo6
+Ol46Xj9IbW1tbW1xczo6Ojo6OjpeOjo6Ojo6Xjo6Ojo6Ojo6Xjo/V21nSFkrOj8K
+ICArOjo6Ol46Oj9UWSs6OjpeOjo6Ol46Ojo6Ojo/WT0/VEhtMzo6Xis6Ol46Ol46
+Xjo6Ojo6XjpeOjo6OkpkbUJZOjo6P2AKICAgOi46Ojo6Ojo6Ojo6Ojo6Ojo6Xjo6
+OjpeOjo6Ojo6OjorOjo6Sjc6Ojo6Ojo6Ol46Ol46Ojo6Xjo6SkgzKzo6Ojo/CiAg
+ICAuKy46Xjo6Ojo6OjpeOjpeOjorOjpeOjo6Ojo6Ojo6Ol46Ojo6Ojo6Xjo6Xjo6
+Ojo6Xjo6OjouZDMrOjouOj8hCiAgICAgICAhLDpeOjpeOjo6Xjo6Ojo6Ojo6Ojo6
+Xjo6Xjo6Xjo6Ojo6OjpeOjo6Ojo6Xjo6OjpeOjo6Ljo6Oi4/YAogICAgICAgICBg
+IS46SisrSisrYVFXSFFhJldXV0hxSEhxSG1nYUo6Xjo6OjpeOi4uLi5KPz8/K0o6
+OjouPyEKICAgICAgICAgICAgID9UV0htbW1tbW1tbW1tbW1tbW1tbW1tbW0rSkpK
+alhIbXFxcXFtbW1xSEgwNyFgICAgICAgICAgIG1jCgpXZWxjb21lIHRvIFNtYXJ0
+T1MgVGVybWluYWwKCgoK'\
+| openssl enc -base64 -d
 
   return $?
 
 }
 
-spec_kvm_iso_copy
+happy_term
+
+
+
+
+             ..z+:+++::;++::++++++:::++::++::::++++zazugmJ,.
+         .,7+::.J+J+?JuxJQgHHHHmQggma+JJx,J&&+JJmmSywXk1+zTHH&,
+       ,:?i+.dWOWHHHHHHHHHHHHHHHHHBB9UTY:?+TWYYWHHHHHHn+?Xe+?T:?..
+    .J::?wXgXWHmmmmmmmmmmmmmHWmmmHI::3::::::::::??WHmmmgmWXmHSi,:?+.
+   ,::.+xYY$WWA&WPdhXmmmmmmmmmHH9=J+:::::::::::::::JmmmgmHQWHC::::::i
+  +::?jQkk++z?QmmHHmmmmmmmmmmmZ?z7::::::::::^:^::^::?UHmmmmmK+:::^:::+.
+ +:?dHmmmmmmmKWmRdYYHmgmmgmgmBC?::::::^:^::^::::^:::::?XHK::?+,:::::::+
+.::JmmmgmmmmmHJmB3::?H9:?Tmm::x::::^::::::::::+::::^::::?T6X+??=+.^:::::
++::?HHHHmmgmmmHk:::::?c::?+=:?jc::^::::^:::^::^::^::::::::::?+jkQm,.^::+
+l::::::?Wmmmmg9::::::::::?XJXDx++J,:.:::^::::^::::::^:^::::::?XmmmmR+,:?
+::^:::::?HgmmH+:::^::^:::::?1;?:+?Y3:++::::^:::^::^::::^:^:::?WmmmmmmmD?
+.:::^:::?Hmmg#=jI:::::^:::::::JQmmdx:::+:?:::::::^:::^::::^:::?WmgmmmD::
+ +:::^:::?HmmD:?:::^:::::^:^?Hmmmmmqs:::::::^::::::^::::::::^:?WmgHY+:?
+  +::::^::?TY+:::^::::^::::::?Y=?THm3::^+::^::^:^:::::^:^::::JdmBY:::?`
+   :.::::::::::::::::^::::^::::::::+:::J7::::::::^::^::::^::JH3+::::?
+    .+.:^:::::::^::^::+::^::::::::::^:::::::^::^:::::^::::.d3+::.:?!
+       !,:^::^:::^:::::::::::^::^::^:::::::^::::::^::::^:::.:::.?`
+         `!.:J++J++aQWHQa&WWWHqHHqHmgaJ:^::::^:....J???+J:::.?!
+             ?TWHmmmmmmmmmmmmmmmmmmmmm+JJJjXHmqqqqmmmqHH07!`          mc
+
+Welcome to SmartOS Terminal
+
